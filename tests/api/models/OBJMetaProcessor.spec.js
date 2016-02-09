0297602bd46ea5c0ece3a289a1bd66a532d2344e
liujialesg <liujialesg@yahoo.com.sg>
2016-02-09 16:55:36 +0800

    [update] changed test to use smaller file

--- tests/api/models/OBJMetaProcessor.spec.js
+++ tests/api/models/OBJMetaProcessor.spec.js
@@ -3,6 +3,6 @@ import OBJMetaProcessor from 'api/models/OBJMetaProcessor';
 
 describe('Process .obj file for vertex and face count', () => {
 	it('Give the right count when supplied with file url', () => {
-    	expect( OBJMetaProcessor.processFile('tests/test_files/test.obj')).deep.equal({ vertex: 15732, face: 5244});
+    	expect( OBJMetaProcessor.processFile('tests/test_files/test.obj')).deep.equal({ vertex: 8, face: 6});
   });
 });
