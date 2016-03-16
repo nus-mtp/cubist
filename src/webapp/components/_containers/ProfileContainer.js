fa5e727154834b6dc64432cb18cb57ff745a7e37
Trong Nhan <imouto1994@gmail.com>
2016-03-16 10:21:44 +0800

    [fix] Add silent fialing mechanism when missing private configuration

---
+++ src/webapp/components/_containers/ProfileContainer.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const CLASS_NAME = 'cb-ctn-profile';
+
+class ProfileContainer extends React.Component {
+  static propTypes = {
+
+  };
+
+  render() {
+    return (
+      <div className={ CLASS_NAME }>
+
+      </div>
+    );
+  }
+}
+
+export default ProfileContainer;
