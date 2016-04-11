ae50ed0738bb377806cf819a57c96616e45b7849
liujialesg <liujialesg@yahoo.com.sg>
2016-04-11 21:16:03 +0800

    [update] Added more tests for model controller

--- tests/api/controllers/ModelController.spec.js
+++ tests/api/controllers/ModelController.spec.js
@@ -174,7 +174,7 @@ describe('Model Controller', () => {
     });
 
     it('Should not call User.findByName() and only call Model.getBrowsePageModels()'
-      + 'with correct arguments if request query has no searchString', () => {
+      + ' with correct arguments if request query has no searchString', () => {
       const req = {
         query: {
           foo: 'bar'
@@ -433,6 +433,352 @@ describe('Model Controller', () => {
         .withArgs('foo', edittedFiles);
 
       ModelController.promise.addSnapshots(req);
+      expect(modelModelMock.verify()).to.be.true;
+    });
+  });
+
+  describe('promise.deleteSnapshot', () => {
+    it('Should validate modelid and and user, and reject invalid values', () => {
+      const req = {
+        params: {
+          modelId: 'foo'
+        },
+        user: {
+          _id: 'bar'
+        },
+        body: {
+          index: 0
+        }
+      };
+
+      const modelModelMock = sandbox.mock(Model)
+        .expects('validate')
+        .returns('test error')
+        .once()
+        .withArgs({ _id: 'foo' });
+
+      const userModelMock = sandbox.mock(User)
+        .expects('validate')
+        .returns(null)
+        .once()
+        .withArgs(req.user);
+
+
+      return ModelController.promise.deleteSnapshot(req).catch(() => {
+        expect(modelModelMock.verify()).to.be.true;
+        expect(userModelMock.verify()).to.be.true;
+        return 'rejected';
+      }).then(result => {
+        expect(result).to.equal('rejected');
+      });
+    });
+
+    it('Should call Model.deleteSnapshot with the correct params', () => {
+      const req = {
+        params: {
+          modelId: 'foo'
+        },
+        user: {
+          _id: 'bar'
+        },
+        body: {
+          index: 0
+        }
+      };
+
+      sandbox.stub(User, 'validate').returns(null);
+      sandbox.stub(Model, 'validate').returns(null);
+
+      const modelModelMock = sandbox.mock(Model)
+        .expects('deleteSnapshot')
+        .returns('')
+        .once()
+        .withArgs('foo', 0);
+
+      ModelController.promise.deleteSnapshot(req);
+      expect(modelModelMock.verify()).to.be.true;
+    });
+  });
+
+  describe('promise.addWalkthrough', () => {
+    it('Should validate modelid and and user, and reject invalid values', () => {
+      const req = {
+        params: {
+          modelId: 'foo'
+        },
+        user: {
+          _id: 'bar'
+        },
+        body: {
+          points: []
+        }
+      };
+
+      const modelModelMock = sandbox.mock(Model)
+        .expects('validate')
+        .returns('test error')
+        .once()
+        .withArgs({ _id: 'foo' });
+
+      const userModelMock = sandbox.mock(User)
+        .expects('validate')
+        .returns(null)
+        .once()
+        .withArgs(req.user);
+
+
+      return ModelController.promise.addWalkthrough(req).catch(() => {
+        expect(modelModelMock.verify()).to.be.true;
+        expect(userModelMock.verify()).to.be.true;
+        return 'rejected';
+      }).then(result => {
+        expect(result).to.equal('rejected');
+      });
+    });
+
+    it('Should call Model.addWalkthrough with modelId and req.body', () => {
+      const req = {
+        params: {
+          modelId: 'foo'
+        },
+        user: {
+          _id: 'bar'
+        },
+        body: {
+          index: 0
+        }
+      };
+
+      sandbox.stub(User, 'validate').returns(null);
+      sandbox.stub(Model, 'validate').returns(null);
+
+      const modelModelMock = sandbox.mock(Model)
+        .expects('addWalkthrough')
+        .returns('')
+        .once()
+        .withArgs('foo', req.body);
+
+      ModelController.promise.addWalkthrough(req);
+      expect(modelModelMock.verify()).to.be.true;
+    });
+  });
+
+  describe('promise.updateWalkthrough', () => {
+    it('Should validate modelid and and user, and reject invalid values', () => {
+      const req = {
+        params: {
+          modelId: 'foo'
+        },
+        user: {
+          _id: 'bar'
+        },
+        body: {
+          walkthrough: {
+            animationMode: undefined,
+            disjointedMode: undefined
+          }
+        }
+      };
+
+      const modelModelMock = sandbox.mock(Model)
+        .expects('validate')
+        .returns('test error')
+        .once()
+        .withArgs({ _id: 'foo' });
+
+      const userModelMock = sandbox.mock(User)
+        .expects('validate')
+        .returns(null)
+        .once()
+        .withArgs(req.user);
+
+
+      return ModelController.promise.updateWalkthrough(req).catch(() => {
+        expect(modelModelMock.verify()).to.be.true;
+        expect(userModelMock.verify()).to.be.true;
+        return 'rejected';
+      }).then(result => {
+        expect(result).to.equal('rejected');
+      });
+    });
+
+    it('Should call updateModelWalkthrough with exact given walkthrough point if point is not disjoint', () => {
+      const update = {
+        animationMode: 'test'
+      };
+      const req = {
+        params: {
+          modelId: 'foo'
+        },
+        user: {
+          _id: 'bar'
+        },
+        body: {
+          index: 0,
+          ...update
+        }
+      };
+
+      sandbox.stub(User, 'validate').returns(null);
+      sandbox.stub(Model, 'validate').returns(null);
+
+      const modelModelMock = sandbox.mock(Model)
+        .expects('updateWalkthrough')
+        .returns('')
+        .once()
+        .withArgs('foo', 0, ...update);
+
+      ModelController.promise.updateWalkthrough(req);
+      expect(modelModelMock.verify()).to.be.true;
+    });
+
+    it('Should change animationMode to Stationary if it is a disjoint point', () => {
+      const update = {
+        animationMode: 'test',
+        disjointMode: true
+      };
+      const req = {
+        params: {
+          modelId: 'foo'
+        },
+        user: {
+          _id: 'bar'
+        },
+        body: {
+          index: 0,
+          ...update
+        }
+      };
+
+      const edittedPoint = { ...update };
+      edittedPoint.animationMode = 'Stationary';
+
+      sandbox.stub(User, 'validate').returns(null);
+      sandbox.stub(Model, 'validate').returns(null);
+
+      const modelModelMock = sandbox.mock(Model)
+        .expects('updateWalkthrough')
+        .returns('')
+        .once()
+        .withArgs('foo', 0, edittedPoint);
+
+      ModelController.promise.updateWalkthrough(req);
+      expect(modelModelMock.verify()).to.be.true;
+    });
+  });
+
+  describe('promise.deleteWalkthrough', () => {
+    it('Should validate modelid and and user, and reject invalid values', () => {
+      const req = {
+        params: {
+          modelId: 'foo'
+        },
+        user: {
+          _id: 'bar'
+        },
+        body: {
+          index: 0
+        }
+      };
+
+      const modelModelMock = sandbox.mock(Model)
+        .expects('validate')
+        .returns('test error')
+        .once()
+        .withArgs({ _id: 'foo' });
+
+      const userModelMock = sandbox.mock(User)
+        .expects('validate')
+        .returns(null)
+        .once()
+        .withArgs(req.user);
+
+
+      return ModelController.promise.deleteWalkthrough(req).catch(() => {
+        expect(modelModelMock.verify()).to.be.true;
+        expect(userModelMock.verify()).to.be.true;
+        return 'rejected';
+      }).then(result => {
+        expect(result).to.equal('rejected');
+      });
+    });
+
+    it('Should call Model.deleteWalkthrough with correct modelId and index', () => {
+      const req = {
+        params: {
+          modelId: 'foo'
+        },
+        user: {
+          _id: 'bar'
+        },
+        body: {
+          index: 0
+        }
+      };
+
+      sandbox.stub(User, 'validate').returns(null);
+      sandbox.stub(Model, 'validate').returns(null);
+
+      const modelModelMock = sandbox.mock(Model)
+        .expects('deleteWalkthrough')
+        .returns('')
+        .once()
+        .withArgs('foo', 0);
+
+      ModelController.promise.deleteWalkthrough(req);
+      expect(modelModelMock.verify()).to.be.true;
+    });
+  });
+
+  describe('promise.addStatisticsPoint', () => {
+    it('Should validate modelid and reject invalid values', () => {
+      const req = {
+        params: {
+          modelId: 'bar'
+        },
+        body: {
+          point: {
+            foo: 'bar'
+          }
+        }
+      };
+
+      const modelModelMock = sandbox.mock(Model)
+        .expects('validate')
+        .returns('test error')
+        .once()
+        .withArgs({ _id: 'bar' });
+
+      return ModelController.promise.addStatisticsPoint(req).catch(() => {
+        expect(modelModelMock.verify()).to.be.true;
+        return 'rejected';
+      }).then(result => {
+        expect(result).to.equal('rejected');
+      });
+    });
+
+    it('Should call Model.addStatisticsPoint with the modelId and point\'s data', () => {
+      const req = {
+        params: {
+          modelId: 'bar'
+        },
+        body: {
+          point: {
+            foo: 'bar'
+          }
+        }
+      };
+
+      sandbox.stub(User, 'validate').returns(null);
+      sandbox.stub(Model, 'validate').returns(null);
+
+      const modelModelMock = sandbox.mock(Model)
+        .expects('addStatisticsPoint')
+        .returns('')
+        .once()
+        .withArgs('bar', req.body.point);
+
+      ModelController.promise.addStatisticsPoint(req);
       expect(modelModelMock.verify()).to.be.true;
     });
   });
